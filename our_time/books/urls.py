cf9b2d9fa41e88895ccab651c8719885587ef764
fantaso <fantaso.code@gmail.com>
2019-12-12 22:45:10 +0100

    fix urls for books and authors

--- our_time/books/urls.py
+++ our_time/books/urls.py
@@ -5,13 +5,13 @@ from .views import BookList, BookCreate, BookDetail, BookUpdate, BookDelete, Aut
 
 app_name = 'books'
 urlpatterns = [
-    path('', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),
     path('create-book/', BookCreate.as_view(), name='book-create'),
     path('<int:pk>/book', BookDetail.as_view(), name='book-detail'),
     path('<int:pk>/update-book', BookUpdate.as_view(), name='book-update'),
     path('<int:pk>/delete-book', BookDelete.as_view(), name='book-delete'),
 
-    path('', AuthorList.as_view(), name='author-list'),
+    path('authors/', AuthorList.as_view(), name='author-list'),
     path('create-author/', AuthorCreate.as_view(), name='author-create'),
     path('<int:pk>/author', AuthorDetail.as_view(), name='author-detail'),
     path('<int:pk>/update-author', AuthorUpdate.as_view(), name='author-update'),
