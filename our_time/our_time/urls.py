0a102a79d192703c72c19ee1d8e32e84ecc2d0e6
fantaso <fantaso.code@gmail.com>
2019-12-18 21:34:58 +0100

    View import in main urls.py reverted back to start from the app name

--- our_time/our_time/urls.py
+++ our_time/our_time/urls.py
@@ -6,6 +6,6 @@ from main.views import DashboardView
 urlpatterns = [
     path('', DashboardView.as_view(), name='main'),
     path('admin/', admin.site.urls),
-    path('todo/', include('todo.urls')),
+    path('todos/', include('todos.urls')),
     path('shelf/', include('books.urls')),
 ]
