46cf226822752d01fef4fac93126a33aace17ec9
fantaso <fantaso.code@gmail.com>
2019-12-23 20:38:54 +0100

    serving static and media files for development in base urls.py

--- our_time/our_time/settings.py
+++ our_time/our_time/settings.py
@@ -1,17 +1,15 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'q_meqsehgc3)o#)yz-_^ugcdbcyg-7r=+@$)7_97va45zxxi+6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
+##################################
+###    APPS                    ###
+##################################
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -50,6 +48,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'django.template.context_processors.media',
             ],
         },
     },
@@ -57,8 +56,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'our_time.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+##################################
+###    DATABASES               ###
+##################################
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -66,8 +66,9 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+##################################
+###    PASSWORD VALIDATION     ###
+##################################
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -75,14 +76,38 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
+##################################
+###    INTERNATIONALIZATION    ###
+##################################
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+##########################
+###    STATIC FILES    ###
+##########################
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+##########################
+###    MEDIA FILES     ###
+##########################
+FILE_UPLOAD_HANDLERS = [
+    "django.core.files.uploadhandler.MemoryFileUploadHandler",
+    "django.core.files.uploadhandler.TemporaryFileUploadHandler",
+]
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+ONE_MB = 1024 ** 2
+FILE_UPLOAD_MAX_MEMORY_SIZE = (25 * ONE_MB)
+
+FILE_UPLOAD_PERMISSIONS = None  # default is None or it can be 0o644 for file permissions
+
+# TODO: in Django says 'MEDIA_URL' must not contain the tailing slash 'media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- our_time/our_time/urls.py
+++ our_time/our_time/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -9,3 +11,19 @@ urlpatterns = [
     path('todos/', include('todos.urls')),
     path('shelf/', include('books.urls')),
 ]
+########################################
+###    STATIC & MEDIA FILES - DEV    ###
+########################################
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     MEDIA_URL = settings.MEDIA_URL.strip('/')
+#     urlpatterns += [
+#         re_path(
+#             rf'^{MEDIA_URL}/(?P<path>.*)$',
+#             static.serve,
+#             {'document_root': settings.MEDIA_ROOT}
+#         )
+#     ]
